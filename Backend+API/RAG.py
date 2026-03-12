22e1a11636b9ed6f0412b0ce26de8be1dfcfc7f5
SergioSerran0 <seserr02@ucm.es>
2026-03-12 18:04:09 +0100

    Añadir hybrid y multiquery

--- Backend+API/RAG.py
+++ Backend+API/RAG.py
@@ -1,4 +1,4 @@
-#pip install langchain langchain-community langchain-ollama langchain-chroma pymupdf beautifulsoup4 sentence-transformers
+#pip install langchain langchain-community langchain-ollama langchain-chroma pymupdf beautifulsoup4 sentence-transformers rank_bm25
 
 import os
 from langchain_ollama.llms import OllamaLLM
@@ -8,13 +8,32 @@ from langchain_chroma import Chroma
 from sentence_transformers import CrossEncoder
 from crearBD import crear_DB
 
+from langchain_community.retrievers import BM25Retriever
+
 RAG = None
 reranker = None
+#usar el modelo que quieras de Ollama, mirar cuales tienes instalado: ollama list, descargar nuevo ollama pull llama3.2
+model = OllamaLLM(model="llama3.2")
+
+def generar_queries(pregunta: str):
+
+    prompt_multiquery = f"""
+    Genera 4 reformulaciones diferentes de la siguiente pregunta
+    para buscar información en una base de conocimiento.
+
+    Pregunta: {pregunta}
+
+    Devuelve solo las preguntas, una por línea.
+    """
+
+    respuesta = model.invoke(prompt_multiquery)
+
+    queries = [q.strip() for q in respuesta.split("\n") if q.strip()]
+
+    return [pregunta] + queries
 
 def crear_RAG():
     global RAG
-    #usar el modelo que quieras de Ollama, mirar cuales tienes instalado: ollama list, descargar nuevo ollama pull llama3.2
-    model = OllamaLLM(model="llama3.2")
 
     #Se crea el modelo de embedings
     embed_model = FastEmbedEmbeddings(model_name="BAAI/bge-small-en-v1.5")
@@ -32,9 +51,31 @@ def crear_RAG():
 
     reranker = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
 
+    docs = vector_store.get()
+
+    bm25_retriever = BM25Retriever.from_texts(
+        docs["documents"]
+    )
+
+    bm25_retriever.k = 10
+
     def retrieve_and_rerank(pregunta: str):
         # 1. Recupera los k candidatos iniciales
-        docs = retriever.invoke(pregunta)
+        #docs = retriever.invoke(pregunta)
+
+        #vector_docs = retriever.invoke(pregunta)
+        #bm25_docs = bm25_retriever.invoke(pregunta)
+
+        #docs = vector_docs + bm25_docs
+
+        queries = generar_queries(pregunta)
+
+        docs = []
+
+        for q in queries:
+            docs.extend(retriever.invoke(q))
+
+        docs = list({doc.page_content: doc for doc in docs}.values())
 
         # 2. Puntúa cada par (pregunta, chunk)
         pares = [[pregunta, doc.page_content] for doc in docs]
